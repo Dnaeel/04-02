0c118ba358571516ae7f18d8994d78585d1964a5
Dnaeel <daneelshurubkov1@gmail.com>
2023-02-07 18:40:33 +0600

    HomW

---
+++ script.js
@@ -0,0 +1,31 @@
+/******** 1 **********/
+/*
+let numbers = [1, 2, 3, 4, 5];
+let result = 0;
+for (let i = 0; i < numbers.length; i++) {
+    result += numbers[i];
+}
+console.log(result);
+*/
+
+/******** 2 **********/
+
+/*
+let i = 1;
+while (i <= 1000){
+    console.log(i);
+    i *= 2;
+}
+*/
+
+/******** 3 **********/
+
+/*
+for (let i = 1; i < 20; i++){
+    if (i % 2 != 0)
+    continue;
+
+    console.log(i);
+}
+*/
+
